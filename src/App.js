7160d8b2f37a7490c528b88e16f4dae60d97c392
Maryna Aponasko <aponasko_mari@gmail.com>
2023-09-20 22:07:40 +0300

    chore: create base structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/layouts/Footer/Footer";
+import Main from "./components/layouts/Main/Main";
+import Header from "./components/layouts/Header/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
