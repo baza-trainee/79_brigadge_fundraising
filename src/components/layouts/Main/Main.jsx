00edc5152e1ea6c35ce46b8f3634fae59e78544d
SergeyTokareff <sergey.v.tokareff@gmail.com>
2023-09-23 11:50:28 +0300

    feat: create ukrainian wings section without adaptive

--- src/components/layouts/Main/Main.jsx
+++ src/components/layouts/Main/Main.jsx
@@ -1,9 +1,12 @@
-import styles from './Main.module.css';
+import UkrainianWings from '../../UkrainianWings/UkrainianWings';
 import About from '../../About/About';
+
+import styles from './Main.module.css';
 
 const Main = () => {
   return (
     <div className={styles.main}>
+      <UkrainianWings />
       <About />
     </div>
   );
