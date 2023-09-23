53e21a4859243069a8a049192a6c5fa75a38e3c3
Maryna Aponasko <aponasko_mari@gmail.com>
2023-09-23 22:35:54 +0300

    feat: add new component

--- src/components/layouts/Main/Main.jsx
+++ src/components/layouts/Main/Main.jsx
@@ -1,10 +1,12 @@
-import styles from './Main.module.css';
-import About from '../../About/About';
+import styles from "./Main.module.css";
+import About from "../../About/About";
+import Importance from "../../Importance/Importance";
 
 const Main = () => {
   return (
     <div className={styles.main}>
       <About />
+      <Importance />
     </div>
   );
 };
