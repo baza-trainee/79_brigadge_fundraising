7d630ef73001d90aa29aa6a752b5bdfe02100c96
Dmitro <yourshows12@gmail.com>
2023-09-23 14:01:43 +0300

    feat: made new section Backfire

---
+++ src/components/Backfire/Backfire.test.jsx
@@ -0,0 +1,6 @@
+import Backfire from "./Backfire";
+import { render } from "@testing-library/react";
+
+test("display Backfire section", () => {
+  render(<Backfire />);
+});
--- src/components/layouts/Main/Main.jsx
+++ src/components/layouts/Main/Main.jsx
@@ -1,10 +1,12 @@
-import styles from './Main.module.css';
-import About from '../../About/About';
+import styles from "./Main.module.css";
+import About from "../../About/About";
+import Backfire from "../../Backfire/Backfire";
 
 const Main = () => {
   return (
     <div className={styles.main}>
       <About />
+      <Backfire />
     </div>
   );
 };
