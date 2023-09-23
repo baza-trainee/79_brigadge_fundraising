1cb6dcf445f6d48797836fcfc5fc7e9af9ec1b9f
SergeyTokareff <sergey.v.tokareff@gmail.com>
2023-09-23 13:33:11 +0300

    feat: create footer and add test

--- src/components/layouts/Footer/Footer.jsx
+++ src/components/layouts/Footer/Footer.jsx
@@ -1,10 +1,25 @@
-import styles from "./Footer.module.css";
+import Container from '../Container/Container';
+import SiteIcon from '../../../assets/img/footer/Site.svg';
+import FacebookIcon from '../../../assets/img/footer/Facebook.svg';
+import LinkedinIcon from '../../../assets/img/footer/Linkedin.svg';
+import TelegramIcon from '../../../assets/img/footer/Telegram.svg';
+
+import styles from './Footer.module.css';
 
 const Footer = () => {
+
   return (
-    <div className={styles.footer}>
-      <p>Footer</p>
-    </div>
+    <footer className={styles.footer}>
+      <Container className={styles['footer-container']}>
+        <div className={styles['footer-social-icons']}>
+          <a href="/#"><img src={SiteIcon} alt='Site' /></a>
+          <a href="/#"><img src={FacebookIcon} alt='Facebook' /></a>
+          <a href="/#"><img src={LinkedinIcon} alt='Linkedin' /></a>
+          <a href="/#"><img src={TelegramIcon} alt='Telegram' /></a>
+        </div>
+        <p className={styles['footer-copyright']}>Розробка Baza Trainee Ukraine 2023 (©) Всі права захищені</p>
+      </Container>
+    </footer>
   );
 };
 
