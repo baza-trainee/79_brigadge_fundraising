1bdb72f7ab154b0a9e1f771a2c2518a2c3669005
Dmitro <yourshows12@gmail.com>
2023-09-25 17:52:15 +0300

    fix: issue with deploy

--- src/components/Wings/Wings.jsx
+++ src/components/Wings/Wings.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import Container from '../layouts/Container/Container';
-import styles from './Wings.module.css';
-import cotton from '../../assets/img/wings/cotton.gif';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import Container from "../layouts/Container/Container";
+import styles from "./Wings.module.css";
+import cotton from "../../assets/img/wings/cotton.gif";
 
 const Wings = () => {
   return (
-    <section className={styles['wings']}>
+    <section className={styles["wings"]}>
       <Container>
-        <div className={styles['wings-wrapper']}>
-          <h2 className={styles['wings-title']}>На Крила</h2>
-          <div className={styles['wings-content-wrapper']}>
-            <div className={styles['wings-block1']}>
-              <p className={styles['wings-block1-text']}>Окриляй злих птахів, знищуй ворогів</p>
+        <div className={styles["wings-wrapper"]}>
+          <h2 className={styles["wings-title"]}>На Крила</h2>
+          <div className={styles["wings-content-wrapper"]}>
+            <div className={styles["wings-block1"]}>
+              <p className={styles["wings-block1-text"]}>
+                Окриляй злих птахів, знищуй ворогів
+              </p>
             </div>
-            <div className={styles['wings-block2']}>
-              <div className={styles['wings-block2-img']}>
-                <div className={styles['wings-block2-content']}>
+            <div className={styles["wings-block2"]}>
+              <div className={styles["wings-block2-img"]}>
+                <div className={styles["wings-block2-content"]}>
                   <img src={cotton} alt="cotton" />
-                  <button className={styles['wings-block2-button']}><a href="#">Донат на крила</a></button>
+                  <button className={styles["wings-block2-button"]}>
+                    <a href="#">Донат на крила</a>
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-          <p className={styles['wings-text']}>
-            Жодний з <span className={styles['span-text']}>БпЛА “Бекфаєр”</span> за рік роботи не був втрачений у бойових діях. Невидимі і недосяжні для ворога.
+          <p className={styles["wings-text"]}>
+            Жодний з <span className={styles["span-text"]}>БпЛА “Бекфаєр”</span>{" "}
+            за рік роботи не був втрачений у бойових діях. Невидимі і недосяжні
+            для ворога.
           </p>
         </div>
       </Container>
